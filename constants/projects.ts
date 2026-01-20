619be15c72d70dc7aebad8a8c0ac486be7605f2f
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-01-20 21:55:39 +0800

    added another project to my porfolio

--- constants/projects.ts
+++ constants/projects.ts
@@ -6,6 +6,14 @@ export const projects = [
     tech: ["React", "Vite", "Tailwind CSS", "GSAP", "SVG"],
     projectURL : "https://github.com/rylsherdamz-rgb/MyPortfolio",
     link: "https://snap-charm.vercel.app/",
+  },{
+    title: "Cadena",
+    description: "Cadena is a comprehensive Web3 ecosystem designed to showcase the transformative power of blockchain through a multi-functional dApp that integrates decentralized messaging, secure national budget tracking, and immutable voting systems. The platform features a private, peer-to-peer messaging interface that eliminates centralized oversight, alongside a pioneering "National Budget Tracker" that utilizes blockchain’s transparency to provide citizens with real-time, tamper-proof audits of government spending. Additionally, Cadena incorporates a decentralized voting module that leverages smart contracts to ensure electoral integrity and a fintech layer for managing digital assets, all unified within a modern Next.js interface to prove how decentralized technology can solve real-world problems in governance, communication, and finance.",
+    image: "/cadena/cadena1",
+    tech: ["React", "NextJS", "Tailwind CSS", "GSAP", "Solidty", "Hardhat",],
+    projectURL : "https://github.com/rylsherdamz-rgb/Cadena",
+    link: "https://cadena-two.vercel.app/",
   },
+
 ];
 
