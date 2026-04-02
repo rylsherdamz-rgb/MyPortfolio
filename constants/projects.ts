0a4d506f752b36646ded8e69e23cf04c7a97b3f1
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-04-02 15:52:14 +0800

    updated the type

--- constants/projects.ts
+++ constants/projects.ts
@@ -1,10 +1,19 @@
-export const projects = [
+export type Project = {
+  title: string
+  description: string
+  image: string
+  tech: string[]
+  link: string
+  projectURL?: string
+}
+
+export const projects: Project[] = [
   {
     title: "SnapCharm",
     description: "A modern web app that lets users enhance photos with animated SVG text effects, creative gradients, and stylish visual elements. Built for performance, interactivity, and responsive design, SnapCharm showcases smooth animations and a minimalist aesthetic.",
     image: "/snap-charm.jpg",
     tech: ["React", "Vite", "Tailwind CSS", "GSAP", "SVG"],
-    projectURL : "https://github.com/rylsherdamz-rgb/MyPortfolio",
+    projectURL: "https://github.com/rylsherdamz-rgb/MyPortfolio",
     link: "https://snap-charm.vercel.app/",
   },
   {
@@ -12,7 +21,7 @@ export const projects = [
     description: `Cadena is a comprehensive Web3 ecosystem designed to showcase the transformative power of blockchain through a multi-functional dApp that integrates decentralized messaging, secure national budget tracking, and immutable voting systems. The platform features a private, peer-to-peer messaging interface that eliminates centralized oversight, alongside a pioneering "National Budget Tracker" that utilizes blockchain’s transparency to provide citizens with real-time, tamper-proof audits of government spending. Additionally, Cadena incorporates a decentralized voting module that leverages smart contracts to ensure electoral integrity and a fintech layer for managing digital assets, all unified within a modern Next.js interface to prove how decentralized technology can solve real-world problems in governance, communication, and finance.`,
     image: "/cadena/cadena.png",
     tech: ["React", "NextJS", "Tailwind CSS", "GSAP", "Solidty", "Hardhat", "Wagmi", "RainbowKit", "Tanstack Query"],
-    projectURL : "https://github.com/rylsherdamz-rgb/Cadena",
+    projectURL: "https://github.com/rylsherdamz-rgb/Cadena",
     link: "https://cadena-two.vercel.app/",
   },
   {
@@ -21,8 +30,5 @@ export const projects = [
     image: "/yuwi/yuwi.png",
     tech: ["React Native", "Expo", "NativeWind", "Gemini API"],
     link: "https://yuwi-website.vercel.app/",
-  },,
-
-
+  },
 ];
-
